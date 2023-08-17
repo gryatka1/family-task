9d505d33b1f6d54009497abde4dc9e91fcc29e4b
gryatka <edameow1@gmail.com>
2023-08-17 21:59:48 +0500

    init work on test data

---
+++ src/Controller/TaskController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/microserviceA', name: 'app_task')]
+class TaskController extends AbstractController
+{
+    #[Route('/', name: 'task_index')]
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/TaskController.php',
+        ]);
+    }
+
+    #[Route('/test', name: 'task_index2')]
+    public function index2(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'test2',
+            'path' => 'src/Controller/TaskController.php',
+        ]);
+    }
+}
